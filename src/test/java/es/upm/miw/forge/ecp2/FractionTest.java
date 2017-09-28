ec94fa789fdf01a91f16792a256bc1f13ac8cc3b
Guillermo Sanz <Guillermo Sanz@DESKTOP-VQF1VN7>
2017-09-28 20:25:23 +0200

    Create UnitTest isEquivalent

--- src/test/java/es/upm/miw/forge/ecp2/FractionTest.java
+++ src/test/java/es/upm/miw/forge/ecp2/FractionTest.java
@@ -8,10 +8,12 @@ import org.junit.Test;
 public class FractionTest {
 
     private Fraction fraction;
+    private Fraction fractionToCompare;
 
     @Before
     public void before() {
     		fraction = new Fraction(1, 2);
+    		fractionToCompare = new Fraction(2, 4);
     }
 
     @Test
@@ -26,4 +28,10 @@ public class FractionTest {
     public void testisImpropia() {
         assertEquals(false, fraction.isImpropia());
     }
+    
+    @Test
+	public void testIsEquivalente() {
+		assertEquals(true, fraction.isEquivalente(fractionToCompare));
+		assertEquals(true, fractionToCompare.isEquivalente(fraction));
+	}
 }
